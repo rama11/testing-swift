1ca3cea8a7d0ded28f769147caafb2cfc67f183b
sinergy-dev <appsinergy@gmail.com>
2020-07-06 11:10:17 +0700

    Initial Commit

---
+++ testing2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing2
+//
+//  Created by Rama Agastya on 06/07/20.
+//  Copyright © 2020 Rama Agastya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
